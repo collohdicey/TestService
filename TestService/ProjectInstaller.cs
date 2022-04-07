dc86c7c132183899dbce102c475867923d80bb18
Collins Njoka <karimicnjoka@gmail.com>
2022-04-07 11:30:06 +0300

    Add project files.

---
+++ TestService/ProjectInstaller.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.ServiceProcess;
+using System.Threading.Tasks;
+
+namespace TestService
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            var process = new ServiceProcessInstaller { Account = ServiceAccount.LocalSystem };
+            var serviceAdmin = new ServiceInstaller
+            {
+                StartType = ServiceStartMode.Manual,
+                ServiceName = "MyServiceName",
+                DisplayName = "My Service Display Name"
+            };
+
+            Installers.Add(process);
+            Installers.Add(serviceAdmin);
+
+            InitializeComponent();
+        }
+    }
+}
